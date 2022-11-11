6a9736ac33257f525c868818e867ed14287ea85f
Misha <Misha@MSI>
2022-11-11 23:31:33 +0300

    Add project files.

---
+++ Metods_and_functions/MaF.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+//#define One
+//#define Two
+
+class distance
+{
+	double a = 0;
+	double b = 0;
+	double c = 0;
+public:
+	double get_a()const
+	{
+		return a;
+	}
+	double get_b()const
+	{
+		return b;
+	}
+	double get_c()const
+	{
+		return c;
+	}
+	void set_a(double a)
+	{
+		this->a = a;
+	}
+	void set_b(double b)
+	{
+		this->b = b;
+	}
+	void set_c(double c)
+	{
+		this->c = c;
+	}
+
+};
+double distances(double a, double b);
+
+
+void main()
+{
+#ifdef One
+	distance A;
+	double a;
+	double b;
+	std::cout << "Enter point A"; std::cin >> a;
+	std::cout << "Enter point B"; std::cin >> b;
+	A.set_a(a);
+	A.set_b(b);
+	if (A.get_a() >= A.get_b()) A.set_c(A.get_a() - A.get_b());
+	else A.set_c(A.get_b() - A.get_a());
+
+	std::cout << "distance: " << A.get_c();
+#endif // One
+#ifdef Two
+	double a;
+	double b;
+	std::cout << "Enter point A: "; std::cin >> a;
+	std::cout << "Enter point B: "; std::cin >> b;
+	std::cout << "distance: " << distances(a, b) << std::endl;
+#endif // Two
+}
+
+double distances(double a, double b)
+{
+	double c;
+	if (a >= b) c = a - b;
+	else c = b - a;
+	return c;
+}
---
+++ PlntroductionToOOP/OOP.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+//#define STRAT_POINT
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRAT_POINT
+	std::cout << "hello oop" << std::endl;
+
+	int a;  // объявление переменной а типа инт
+	point a;  //объявление переменной а типа point
+			//создание экземпляра а структуры point
+			//объявление экземпляра а структуры point
+
+	a.x = 2;
+	a.y = 3;
+	std::cout << a.x << "\t" << a.y << std::endl;
+
+	point* pa = &a;
+	std::cout << pa->x << "\t" << pa->y << std::endl;
+#endif // STR
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
+
+}
