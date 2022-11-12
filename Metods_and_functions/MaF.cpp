4f6a3475bb76fca8921b8deb64b71a7bc01cde0a
Misha <Misha@MSI>
2022-11-13 01:58:37 +0300

    Finished

--- Metods_and_functions/MaF.cpp
+++ Metods_and_functions/MaF.cpp
@@ -2,6 +2,7 @@
 
 //#define One
 //#define Two
+//#define three
 
 class distance
 {
@@ -33,6 +34,23 @@ public:
 	{
 		this->c = c;
 	}
+	double dist(double a, double b)
+	{
+		
+		if (get_a() >= get_b()) set_c(get_a() - get_b());
+		else set_c(get_b() - get_a());
+		return c;
+
+	}
+	void print(double a, double b)
+	{
+		//distance C;
+		std::cout << "Enter point A: "; std::cin >> a;
+		std::cout << "Enter point B: "; std::cin >> b;
+		set_a(a);
+		set_b(b);
+	}
+
 
 };
 double distances(double a, double b);
@@ -44,8 +62,8 @@ void main()
 	distance A;
 	double a;
 	double b;
-	std::cout << "Enter point A"; std::cin >> a;
-	std::cout << "Enter point B"; std::cin >> b;
+	std::cout << "Enter point A: "; std::cin >> a;
+	std::cout << "Enter point B: "; std::cin >> b;
 	A.set_a(a);
 	A.set_b(b);
 	if (A.get_a() >= A.get_b()) A.set_c(A.get_a() - A.get_b());
@@ -60,7 +78,16 @@ void main()
 	std::cout << "Enter point B: "; std::cin >> b;
 	std::cout << "distance: " << distances(a, b) << std::endl;
 #endif // Two
+#ifdef three
+	distance B;
+	double a = 0;
+	double b = 0;
+	B.print(a, b);
+	B.dist(a, b);
+	std::cout << "distance: " << B.dist(a,b);
 }
+#endif // three
+
 
 double distances(double a, double b)
 {
